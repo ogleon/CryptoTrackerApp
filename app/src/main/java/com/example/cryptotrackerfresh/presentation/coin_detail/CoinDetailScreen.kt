9d023ded263a6ed82e56b15f9d544f3be831f08b
lion <leonn.matte@gmail.com>
2021-12-21 22:09:02 +0100

    Details page looks better

--- app/src/main/java/com/example/cryptotrackerfresh/presentation/coin_detail/CoinDetailScreen.kt
+++ app/src/main/java/com/example/cryptotrackerfresh/presentation/coin_detail/CoinDetailScreen.kt
@@ -6,17 +6,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -47,163 +42,196 @@ fun CoinDetailScreen(
         .background(colorResource(id = background))) {
         state.coin?.let { coin ->
             LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.SpaceBetween
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceBetween,
             ) {
                 item {
-
-                    Column(modifier = Modifier
+                    Surface(modifier = Modifier
                         .padding(15.dp)
-                        .wrapContentSize()
-                        .background(colorResource(id = backgroundCard), RoundedCornerShape(6.dp)),
-                    verticalArrangement = Arrangement.SpaceAround) {
+                        .fillMaxWidth()
+                        .background(color = colorResource(id = backgroundCard))
+                        .fillParentMaxHeight(),
+                        elevation = 8.dp) {
 
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically) {
-                            Text(text = coin.last_updated,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(end = 8.dp)
-                                    .wrapContentHeight(),
-                                color = colorResource(id = R.color.gray),
-                                textAlign = TextAlign.Center,
-                                fontSize = 15.sp)
-                        }
+                        Column(modifier = Modifier
+                            .fillParentMaxHeight()
+                            .padding(10.dp),
+                            verticalArrangement = Arrangement.Top) {
 
-                        Row(modifier = Modifier
-                            .padding(4.dp),
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.SpaceBetween) {
-
-                            val imageLoader = ImageLoader.Builder(LocalContext.current)
-                                .componentRegistry {
-                                    add(SvgDecoder(LocalContext.current))
-                                }
-                                .error(R.drawable.ic_no_image_found4)
-                                .build()
-
-                            Column(
-                                modifier = Modifier
-                                    .wrapContentSize()
-                                    .padding(5.dp),
-                                horizontalAlignment = Alignment.Start,
-                                verticalArrangement = Arrangement.Center) {
-
-                                CompositionLocalProvider(LocalImageLoader.provides(imageLoader)) {
-                                    val painter =
-                                        rememberImagePainter(Constants.IMG_URL + coin.name.lowercase() + "-" + coin.symbol.lowercase() + "-logo.svg?v=002")
-
-                                    val state0 = painter.state
-                                    Box(
-                                        contentAlignment = Alignment.Center,
-                                        modifier = Modifier.wrapContentSize()
-                                    ) {
-                                        this@Column.AnimatedVisibility(visible = (state0 is ImagePainter.State.Loading)) {
-                                            CircularProgressIndicator()
-                                        }
-                                        Image(
-                                            painter = painter,
-                                            contentDescription = null,
-                                            modifier = Modifier
-                                                .size(64.dp)
-                                                .wrapContentSize(),
-                                            contentScale = ContentScale.Fit,
-                                            alignment = Alignment.CenterStart
-
-                                        )
-                                    }
-
-                                }
-                            }
-
-                            Column(modifier = Modifier
-                                .fillMaxWidth()
-                                .wrapContentHeight()
-                                .padding(5.dp),
-                                horizontalAlignment = Alignment.Start) {
-                                Text(
-                                    text = coin.symbol,
-                                    color = colorResource(id = R.color.black),
-                                    fontWeight = FontWeight.Bold)
-                                Text(text = coin.name,
-                                    color = colorResource(R.color.gray),
+                            Row {
+                                Text(text = coin.last_updated,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(end = 8.dp)
+                                        .wrapContentHeight(),
+                                    color = colorResource(id = R.color.gray),
+                                    textAlign = TextAlign.End,
                                     fontSize = 15.sp)
                             }
 
+                            Row(modifier = Modifier
+                                .padding(2.dp)) {
+
+                                val imageLoader = ImageLoader.Builder(LocalContext.current)
+                                    .componentRegistry {
+                                        add(SvgDecoder(LocalContext.current))
+                                    }
+                                    .error(R.drawable.ic_no_image_found4)
+                                    .build()
+
+                                Column(
+                                    modifier = Modifier
+                                        .wrapContentSize()
+                                        .padding(5.dp),
+                                    horizontalAlignment = Alignment.Start) {
+
+                                    CompositionLocalProvider(LocalImageLoader.provides(imageLoader)) {
+                                        val painter =
+                                            rememberImagePainter(Constants.IMG_URL + coin.name.lowercase() + "-" + coin.symbol.lowercase() + "-logo.svg?v=002")
+
+                                        val state0 = painter.state
+                                        Box(
+                                            contentAlignment = Alignment.Center,
+                                            modifier = Modifier.wrapContentSize()
+                                        ) {
+                                            this@Column.AnimatedVisibility(visible = (state0 is ImagePainter.State.Loading)) {
+                                                CircularProgressIndicator()
+                                            }
+                                            Image(
+                                                painter = painter,
+                                                contentDescription = null,
+                                                modifier = Modifier
+                                                    .size(64.dp)
+                                                    .wrapContentSize(),
+                                                contentScale = ContentScale.Fit,
+                                                alignment = Alignment.CenterStart
+
+                                            )
+                                        }
+
+                                    }
+                                }
+
+                                Column(modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentHeight()
+                                    .padding(4.dp),
+                                    horizontalAlignment = Alignment.Start) {
+                                    Text(
+                                        text = coin.symbol,
+                                        color = colorResource(id = R.color.black),
+                                        fontWeight = FontWeight.ExtraBold)
+                                    Text(text = coin.name,
+                                        color = colorResource(R.color.gray),
+                                        fontWeight = FontWeight.Normal,
+                                        fontSize = 15.sp)
+                                }
+                            }
+
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxSize()) {
+
+                                Column(modifier = Modifier
+                                    .fillMaxSize(),
+                                    verticalArrangement = Arrangement.SpaceEvenly) {
+
+                                    Text(text = "Rank", modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight(),
+                                        colorResource(id = R.color.black),
+                                        fontWeight = FontWeight.ExtraBold,
+
+                                        textAlign = TextAlign.Center)
+
+                                    Text(text = coin.rank, modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight(),
+                                        colorResource(id = R.color.gray),
+                                        fontWeight = FontWeight.Normal,
+
+                                        textAlign = TextAlign.Center)
+
+                                    Text(text = "Price", modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight(),
+                                        colorResource(id = R.color.black),
+                                        fontWeight = FontWeight.ExtraBold,
+                                        textAlign = TextAlign.Center)
+
+                                    Text(text = "$" + coin.price, modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight(),
+                                        colorResource(id = R.color.gray),
+                                        fontWeight = FontWeight.Normal,
+                                        textAlign = TextAlign.Center)
+
+                                    Text(text = "Market Cap", modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight(),
+                                        colorResource(id = R.color.black),
+                                        fontWeight = FontWeight.ExtraBold,
+                                        textAlign = TextAlign.Center)
+
+                                    Text(text = coin.marketCap, modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight(),
+                                        colorResource(id = R.color.gray),
+                                        fontWeight = FontWeight.Normal,
+                                        textAlign = TextAlign.Center)
+
+
+                                    Text(text = "Max Supply", modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight(),
+                                        colorResource(id = R.color.black),
+                                        fontWeight = FontWeight.ExtraBold,
+                                        textAlign = TextAlign.Center)
+
+                                    Text(text = coin.maxSupply, modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight(),
+                                        colorResource(id = R.color.gray),
+                                        fontWeight = FontWeight.Normal,
+                                        textAlign = TextAlign.Center)
+
+
+                                    Text(text = "Total Supply", modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight(),
+                                        colorResource(id = R.color.black),
+                                        fontWeight = FontWeight.ExtraBold,
+                                        textAlign = TextAlign.Center)
+
+                                    Text(text = coin.totalSupply, modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight(),
+                                        colorResource(id = R.color.gray),
+                                        fontWeight = FontWeight.Normal,
+                                        textAlign = TextAlign.Center)
+
+                                    Text(text = "Circulating Supply", modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight(),
+                                        colorResource(id = R.color.black),
+                                        textAlign = TextAlign.Center,
+                                        fontWeight = FontWeight.ExtraBold
+                                    )
+
+                                    Text(text = coin.circulatingSupply, modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight(),
+                                        colorResource(id = R.color.gray),
+                                        fontWeight = FontWeight.Normal,
+                                        textAlign = TextAlign.Center)
+                                }
+
+                            }
 
                         }
 
-                        Text(text = "Rank", modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                            colorResource(id = R.color.gray),
-                            textAlign = TextAlign.Center)
-
-                        Text(text = coin.rank, modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                            textAlign = TextAlign.Center)
-
-                        Text(text = "Price", modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                            colorResource(id = R.color.gray),
-                            textAlign = TextAlign.Center)
-
-                        Text(text = "$" + coin.price, modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                            textAlign = TextAlign.Center)
-
-                        Text(text = "Market Cap", modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                            colorResource(id = R.color.gray),
-                            textAlign = TextAlign.Center)
-
-                        Text(text = coin.marketCap, modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                            textAlign = TextAlign.Center)
-
-
-                        Text(text = "Max Supply", modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                            colorResource(id = R.color.gray),
-                            textAlign = TextAlign.Center)
-
-                        Text(text = coin.maxSupply, modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                            textAlign = TextAlign.Center)
-
-
-                        Text(text = "Total Supply", modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                            colorResource(id = R.color.gray),
-                            textAlign = TextAlign.Center)
-
-                        Text(text = coin.totalSupply, modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                            textAlign = TextAlign.Center)
-
-
-                        Text(text = "Circulating Supply", modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                            colorResource(id = R.color.gray),
-                            textAlign = TextAlign.Center)
-
-                        Text(text = coin.circulatingSupply, modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                            textAlign = TextAlign.Center)
-
                     }
-
                 }
             }
         }
